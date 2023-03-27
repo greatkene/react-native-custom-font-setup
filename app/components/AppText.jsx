cc265d0d43269045ba90a91c27c54e22e1bef0b6
greatkene <ihevuemeg@gmail.com>
2023-03-27 17:41:11 +0100

    initial commit

---
+++ app/components/AppText.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AppText = () => {
+  return (
+    <View>
+      <Text>AppText</Text>
+    </View>
+  );
+};
+
+export default AppText;
+
+const styles = StyleSheet.create({});
